417111faa0980c3df02d213038bafb8f0b869136
FPNBR <felipepny@gmail.com>
2023-03-16 20:39:30 -0300

    Configurando JPA + Banco de dados

--- src/main/java/br/com/fpnbr/springbootmvc/SpringBootMvcApplication.java
+++ src/main/java/br/com/fpnbr/springbootmvc/SpringBootMvcApplication.java
@@ -2,8 +2,10 @@ package br.com.fpnbr.springbootmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "br.com.fpnbr.springbootmvc.models")
 public class SpringBootMvcApplication {
 
 	public static void main(String[] args) {
