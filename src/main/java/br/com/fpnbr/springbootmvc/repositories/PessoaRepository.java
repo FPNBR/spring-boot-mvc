2867557211ce55ebb08a53980164855016657c16
FPNBR <felipepny@gmail.com>
2023-03-18 18:09:48 -0300

    Ajuste no layout e na consulta JPQL

--- src/main/java/br/com/fpnbr/springbootmvc/repositories/PessoaRepository.java
+++ src/main/java/br/com/fpnbr/springbootmvc/repositories/PessoaRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 @Repository
 @Transactional
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-    @Query("select p from Pessoa p where p.nome like %?1%")
+    @Query("select p from Pessoa p where lower(concat(p.nome, ' ', p.sobrenome)) like lower(concat('%', ?1, '%'))")
     List<Pessoa> findPessoaByName(String nome);
 }
